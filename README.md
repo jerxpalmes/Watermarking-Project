1efec289cf12d03a2b99995736490bd6d9c7ceb8
joshpalacios <95915521+joshpalacios@users.noreply.github.com>
2022-01-12 15:11:25 +0800

    change name

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Watermarking-Project
+# Watermarking-Project project used to watermark multiple images 
---
+++ final.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import glob
+import os
+
+from numpy._distributor_init import filename
+
+logo = cv2.imread("elite_logo.jpg") #loading logo image
+h_logo, w_logo, _ = logo.shape #getting height and width
+
+images_path = glob.glob("watermarking/*.*")
+print("Adding watermark")
+
+for img_path in images_path:
+    img = cv2.imread(img_path)
+    h_img, w_img, _ = img.shape
+    center_y = int(h_img / 2)
+    center_x = int(w_img / 2)
+    top_y = center_y - int(h_logo / 2)
+    left_x = center_x - int(w_logo / 2)
+    bottom_y = top_y + h_logo
+    right_x = left_x + w_logo
+
+    # Extracting the ROI
+    roi = img[top_y:bottom_y, left_x:right_x]
+
+    result = cv2.addWeighted(roi, 1, logo, 0.3, 0)
+    img[top_y:bottom_y, left_x:right_x] = result
+    filename= os.path.basename(img_path)
+
+    cv2.imwrite("watermarked_images/watermarked_" + filename, img)
+
+print("Watermark added to all images")
+# print("collab")
